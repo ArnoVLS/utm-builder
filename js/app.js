e4a606cfd8a815ebaa50de78d2fe09041cb5e29f
ArnoVLS <arno.vlasschaert@agoria.be>
2026-09-01 10:12:58 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,3 @@
-const supabaseUrl = 'https://hnsbjmomaynwsfmoelun.supabase.co';
-const supabaseKey = 'sb_secret_TBDj_1r6raa1yLBWofw2ow_YMovLAoN';
-
-const supabaseClient = supabase.createClient(
-  supabaseUrl,
-  supabaseKey
-);
-
 async function test() {
 
   const { data, error } = await supabaseClient
