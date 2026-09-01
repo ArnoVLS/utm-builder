a79286d42d0414d51bc7173de5257c43ed1b68e2
ArnoVLS <arno.vlasschaert@agoria.be>
2026-09-01 10:13:20 +0200

    Initialize Supabase client with URL and key

---
+++ js/supabase.js
@@ -0,0 +1,7 @@
+const supabaseUrl = 'https://hnsbjmomaynwsfmoelun.supabase.co';
+const supabaseKey = 'sb_publishable_5wlGQ4-EZhR5gkcDDsiefw__fyZfjHk';
+
+const supabaseClient = supabase.createClient(
+  supabaseUrl,
+  supabaseKey
+);
