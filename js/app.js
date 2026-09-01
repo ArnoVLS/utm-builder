2e6bc62413bb0beb389e066c582a124e8999dee5
ArnoVLS <arno.vlasschaert@agoria.be>
2026-09-01 10:02:52 +0200

    Initialize Supabase client and fetch data

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
+const supabaseUrl = 'https://hnsbjmomaynwsfmoelun.supabase.co';
+const supabaseKey = 'sb_secret_TBDj_1r6raa1yLBWofw2ow_YMovLAoN';
 
+const supabaseClient = supabase.createClient(
+  supabaseUrl,
+  supabaseKey
+);
+
+async function test() {
+
+  const { data, error } = await supabaseClient
+    .from('sources')
+    .select('*');
+
+  console.log(data);
+
+}
+
+test();
